c0e4dc48ecf355ae616300d9554b0418fde1cac4
Austin Erlandson <austin@erlandson.com>
2015-05-19 23:00:54 -0500

    Got rid of begin,rescue,end syntax cause sql's...
    default error handling is to ABORT which leaves the database in it's
    current state and throws an error when it happens. Which is good for the
    security of a recording.

--- lib/serial_files/spike_sql_batch_insert_test_file.rb
+++ lib/serial_files/spike_sql_batch_insert_test_file.rb
@@ -2,28 +2,19 @@
 
 require 'sqlite3'
 
-begin
+database = SQLite3::Database.open "db/serial_parser_storage_test.sqlite"
+database.execute("DELETE FROM playground")
 
-  database = SQLite3::Database.open "db/serial_parser_storage_test.sqlite"
-  database.execute("DELETE FROM sessions")
-
-  database.transaction do |db|
-    sql = db.prepare "INSERT INTO [sessions] (id, sensor_id, pressure_value, time_stamp) VALUES (?, ?, ?, strftime('%Y-%m-%d %H:%M:%f'));"
-    File.foreach('./lib/serial_files/test_serial_raw_80_seconds.txt') do |line|
-      temp_arr = line.split(':')
-      sql.execute(1, temp_arr[0].to_i(16) + 1, temp_arr[1].to_i(16) + 1)
-    end
+database.transaction do |db|
+  sql = db.prepare "INSERT INTO [playground] (id, sensor_id, pressure_value, time_stamp) VALUES (?, ?, ?, strftime('%Y-%m-%d %H:%M:%f'));"
+  File.foreach('./lib/serial_files/test_serial_raw_80_seconds.txt').first(50).each do |line|
+    temp_arr = line.split(':')
+    sql.execute(1, temp_arr[0].hex + 1, temp_arr[1].hex + 1)
   end
 
-rescue SQLite3::Exception => e
-
-    puts "Exception occurred"
-    puts e
-    database.rollback
-
-ensure
-
-    sql.close if sql
-    database.close if database
+  File.foreach('./lib/serial_files/test_serial_raw_15_seconds.txt').first(50).each do |line|
+    temp_arr = line.split(':')
+    sql.execute(2, temp_arr[0].hex + 1, temp_arr[1].hex + 1)
+  end
 
 end
