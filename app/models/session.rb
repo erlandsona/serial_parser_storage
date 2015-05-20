304df7c6232c87299ac0dad2edfc5a55455b57e6
Austin Erlandson <austin@erlandson.com>
2015-05-19 22:57:29 -0500

    Session.find functioning and tested.
    Moving on to Session.record

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,7 +1,18 @@
 class Session
 
-  def self.all
-    Database.execute("SELECT * FROM sessions")
+  def self.find(id: nil, sensor_id: nil, pressure_value: nil)
+    statement = "SELECT * FROM sessions"
+    arguments = {id: id, sensor_id: sensor_id, pressure_value: pressure_value}.reject{|k, v| v.nil?}
+    unless arguments.empty?
+      statement << " WHERE"
+      statement << arguments.map { |key, val| " #{key.to_s}=#{val.to_s}" }.join(' AND')
+    end
+    statement << ";"
+    Database.execute(statement)
+  end
+
+  def self.pragma
+    Database.execute("PRAGMA sessions")
   end
 
 end
--- test/models/test_session.rb
+++ test/models/test_session.rb
@@ -2,26 +2,60 @@ require_relative '../test_helper'
 
 describe Session do
 
-  describe "#all" do
+  describe "#find" do
 
-    describe "if there are no sessions in the database" do
+    describe "if there are no results" do
 
       it "should return an empty array" do
-        assert_equal [], Session.all
+        assert_equal [], Session.find
       end
 
     end
 
-    describe "if there are sessions" do
+    describe "if there are results" do
 
-      before do
+      before(:all) do
         record_test_sessions
       end
 
-      it "should return the sessions" do
-        expected = ["Amanda", "Bob", "Sally"]
-        actual = Session.all
-        assert_equal expected, actual
+      it "should return the correct number of rows for no arguments given" do
+        session = Session.find
+        assert_equal 20, session.length
+      end
+
+      it "should return the correct number of rows for id" do
+        session = Session.find(id: 1)
+        assert_equal 10, session.length
+      end
+
+      it "should return the correct number of rows for sensor_id" do
+        session = Session.find(sensor_id: 1)
+        assert_equal 1, session.length
+      end
+
+      it "should return the correct number of rows for pressure_value" do
+        session = Session.find(pressure_value: 1)
+        assert_equal 17, session.length
+      end
+
+      it "should return the correct number of rows for id AND sensor_id" do
+        session = Session.find(id: 1, sensor_id: 2)
+        assert_equal 1, session.length
+      end
+
+      it "should return the correct number of rows for id AND pressure_value" do
+        session = Session.find(id: 1, pressure_value: 1)
+        assert_equal 8, session.length
+      end
+
+      it "should return the correct number of rows for sensor_id AND pressure_value" do
+        session = Session.find(sensor_id: 3, pressure_value: 1)
+        assert_equal 2, session.length
+      end
+
+      it "should return the correct number of rows for all args" do
+        session = Session.find(id: 1, sensor_id:3, pressure_value: 1)
+        assert_equal 1, session.length
       end
 
     end
